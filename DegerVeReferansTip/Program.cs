46cb95fe3345def22a9d75e01aa2f59a92e73ca8
EREN ÖZTÜRK <erenozturk5772@gmail.com>
2021-01-22 06:04:59 +0300

    20 Ocak Ders Sonu

--- DegerVeReferansTip/Program.cs
+++ DegerVeReferansTip/Program.cs
@@ -11,7 +11,8 @@ namespace DegerVeReferansTip
             int sayi1 = 10;
             int sayi2 = 30;
             sayi1 = sayi2;
-            sayi2 = 65
+            sayi2 = 65;
+                      
         }
     }
 }
---
+++ OOP1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "masa";
+            product1.UnitPrice = 500;
+            product1.UnitInStock = 3;
+
+            Product product = new Product {Id=2, CategoryId=5, ProductName = "kalem", UnitPrice= 35, UnitInStock = 5 };
+
+            //PascalCase //camelCase
+            //case sensitive 
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            
+
+            // int, bool, double... değer tip 
+            // diziler, class, abstract class, interface ... referans tip
+
+
+        }
+    }
+}
